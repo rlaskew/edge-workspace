30c320e94538d747fbd1ebce91cda3ae3573fa2d
rlaskew <richardaskew@yahoo.com>
2021-11-11 08:46:42 -0600

    Update helpful-aws-commands.sh

--- helpful-aws-commands.sh
+++ helpful-aws-commands.sh
@@ -4,3 +4,4 @@ acf validate-template --template-body file://vpc.yaml
 acf create-stack --template-body file://vpc.yaml --stack-name myvpc
 acf describe-stack-events --stack-name myvpc
 aws ec2 describe-vpcs
+acf delete-stack --stack-name myvpc
