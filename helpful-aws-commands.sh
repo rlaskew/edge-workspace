f97bfa5cbd9d79ae7e4e6b5b84858b5a5f6c7747
Your Name <you@example.com>
2022-01-12 16:50:10 +0000

    Checkpoint

--- helpful-aws-commands.sh
+++ helpful-aws-commands.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 alias acf='aws cloudformation'
 acf validate-template --template-body file://vpc.yaml
-acf create-stack --template-body file://vpc.yaml --stack-name myvpc
+# acf create-stack --template-body file://vpc.yaml --stack-name myvpc
+acf create-stack --template-body file://vpc.yaml --stack-name myvpc --region us-east-1 --parameter ParameterKey=EnvironmentName,ParameterValue=dev
 acf describe-stack-events --stack-name myvpc
 aws ec2 describe-vpcs
 acf delete-stack --stack-name myvpc
