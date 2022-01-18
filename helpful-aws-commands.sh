91874c4cdce79c391191d96442804759ab2e72ab
Your Name <you@example.com>
2022-01-18 21:49:06 +0000

    Updated ec2.yaml and useful commands to validate ec2.yaml before merging ec2 and vpc cft

--- helpful-aws-commands.sh
+++ helpful-aws-commands.sh
@@ -12,3 +12,9 @@ acf create-stack --template-body file://vpc.yaml --stack-name myvpc --region us-
 acf describe-stack-events --stack-name myvpc
 aws ec2 describe-vpcs
 acf delete-stack --stack-name myvpc
+
+## useful for seeding parameters
+aws ec2 describe-subnets | jq .Subnets[0]
+
+## create ec2
+acf create-stack --template-body file://ec2.yaml --region us-east-1 --stack-name ec2-take1 --parameter ParameterKey=KeyNameParameter,ParameterValue=MyKeyPair --capabilities CAPABILITY_IAM
