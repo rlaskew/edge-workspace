089f7221e9ec62203027130553a2224ea49da911
Your Name <you@example.com>
2022-01-27 22:18:10 +0000

    minor update/progress on demo.py

--- scripts/demo.py
+++ scripts/demo.py
@@ -5,14 +5,32 @@
  
 import time
 import requests
+import json
+#!/usr/bin/python3
+
+# TODO: above needs updating
+# Python program to calculate RTT
+ 
+import time
+import requests
+import json
  
 # Function to calculate the RTT
 def RTT(url):
- 
+    # payload
+    payload = {
+              "key1": "value1"
+            }
+
+    # headers
+    headers = {
+            'key1':"Value1"
+            }
     # time when the signal is sent
     t1 = time.time()
- 
-    r = requests.get(url)
+
+    #r = requests.get(url)
+    r = requests.post(url,data=payload,headers=headers)
  
     # time when acknowledgement of signal
     # is received
@@ -22,7 +40,10 @@ def RTT(url):
     tim = str(t2-t1)
  
     print("Time in seconds :" + tim)
+    print("Response:" + r.text)
     time.sleep(1) 
+    data = json.loads(r.text)
+    print(data["form"]["key1"])
 # driver program
 # url address
 #url = "http://www.google.com"
