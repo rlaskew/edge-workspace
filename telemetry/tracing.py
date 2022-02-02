3d9eec12fbd1326c4a4aa67f8f3efcb395729abb
Your Name <you@example.com>
2022-02-02 21:38:39 +0000

    extending Otel code with trace attributes experiments

--- telemetry/tracing.py
+++ telemetry/tracing.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # tracing.py
-from opentelemetry import trace
+from opentelemetry import trace,baggage
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
             BatchSpanProcessor,
@@ -15,7 +15,14 @@ trace.set_tracer_provider(provider)
 
 tracer = trace.get_tracer(__name__)
 
-with tracer.start_as_current_span("foo"):
-        with tracer.start_as_current_span("bar"):
-                 with tracer.start_as_current_span("baz"):
-                                    print("Hello world from OpenTelemetry Python!")
+## original for reference
+#with tracer.start_as_current_span("foo"):
+#     with tracer.start_as_current_span("bar"):
+#          with tracer.start_as_current_span("baz"):
+#                print("Hello world from OpenTelemetry Python!")
+
+context_demo = baggage.set_baggage("context","global")
+## https://opentelemetry-python.readthedocs.io/en/latest/sdk/trace.html
+## https://opentelemetry.io/docs/reference/specification/overview/
+with tracer.start_as_current_span("baz",context=context_demo,attributes={"foo":"bar","bar":"foo","random":1}):
+     print("Hello world from OpenTelemetry Python!")
